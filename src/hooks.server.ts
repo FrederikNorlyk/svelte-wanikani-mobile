56b72209570b9b778cc8cc373592fb9cfe56c49b
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-25 20:59:33 +0100

    Formatting with prettier

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,19 @@
-import CookieUtil from "$lib/util/cookieUtil";
-import type { Handle } from "@sveltejs/kit";
-import { redirect } from "@sveltejs/kit";
+import CookieUtil from '$lib/util/cookieUtil';
+import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const routeId = event.route.id;
+	const routeId = event.route.id;
 
-  const apiToken = CookieUtil.get(event.cookies, "api_token");
+	const apiToken = CookieUtil.get(event.cookies, 'api_token');
 
-  if (routeId?.startsWith("/(authed)")) {
-    if (!apiToken) {
-      throw redirect(303, "/login");
-    }
-  } else if (apiToken) {
-    throw redirect(303, "/");
-  }
+	if (routeId?.startsWith('/(authed)')) {
+		if (!apiToken) {
+			throw redirect(303, '/login');
+		}
+	} else if (apiToken) {
+		throw redirect(303, '/');
+	}
 
-  return resolve(event);
+	return resolve(event);
 };
--- src/lib/util/cookieUtil.ts
+++ src/lib/util/cookieUtil.ts
@@ -1,22 +1,22 @@
-import type { Cookies } from "@sveltejs/kit";
+import type { Cookies } from '@sveltejs/kit';
 
-export type CookieKey = "api_token";
+export type CookieKey = 'api_token';
 
 export default class CookieUtil {
-  public static get(cookies: Cookies, key: CookieKey): string | undefined {
-    return cookies.get(key);
-  }
+	public static get(cookies: Cookies, key: CookieKey): string | undefined {
+		return cookies.get(key);
+	}
 
-  public static set(cookies: Cookies, key: CookieKey, value: string) {
-    cookies.set(key, value, {
-      path: "/",
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax",
-    });
-  }
+	public static set(cookies: Cookies, key: CookieKey, value: string) {
+		cookies.set(key, value, {
+			path: '/',
+			httpOnly: true,
+			secure: true,
+			sameSite: 'lax'
+		});
+	}
 
-  public static delete(cookies: Cookies, key: CookieKey) {
-    cookies.delete(key, { path: "/" });
-  }
+	public static delete(cookies: Cookies, key: CookieKey) {
+		cookies.delete(key, { path: '/' });
+	}
 }
