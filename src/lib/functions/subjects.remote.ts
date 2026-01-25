eb22b40664c8fea506db77d2811ce95ee9eefbb5
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-25 20:57:20 +0100

    Added fetching, caching, and retrieval of subjects.

--- src/lib/functions/subjects.remote.ts
+++ src/lib/functions/subjects.remote.ts
@@ -1,69 +1,79 @@
-import { getRequestEvent, query } from "$app/server";
-import CookieUtil from "$lib/util/cookieUtil";
-import * as v from "valibot";
+import { getRequestEvent, query } from '$app/server';
+import CookieUtil from '$lib/util/cookieUtil';
+import * as v from 'valibot';
 
 const subjectSchema = v.pipe(
-  v.object({
-    id: v.number(),
-    object: v.picklist(["radical", "kanji", "vocabulary"]),
-    data: v.object({
-      characters: v.string(),
-      meanings: v.array(
-        v.object({
-          meaning: v.string(),
-          primary: v.boolean(),
-          accepted_answer: v.boolean(),
-        }),
-      ),
-      readings: v.array(
-        v.object({
-          type: v.picklist(["onyomi", "kunyomi", "nanori"]),
-          primary: v.boolean(),
-          accepted_answer: v.boolean(),
-          reading: v.string(),
-        }),
-      ),
-    }),
-  }),
-  v.transform((data) => ({
-    id: data.id,
-  })),
+	v.object({
+		id: v.number(),
+		object: v.picklist(['radical', 'kanji', 'vocabulary']),
+		data: v.object({
+			characters: v.string(),
+			meanings: v.array(
+				v.object({
+					meaning: v.string(),
+					primary: v.boolean(),
+					accepted_answer: v.boolean()
+				})
+			),
+			readings: v.array(
+				v.object({
+					type: v.picklist(['onyomi', 'kunyomi', 'nanori']),
+					primary: v.boolean(),
+					accepted_answer: v.boolean(),
+					reading: v.string()
+				})
+			)
+		})
+	}),
+	v.transform((data) => ({
+		id: data.id,
+		object: data.object,
+		characters: data.data.characters,
+		meanings: data.data.meanings,
+		readings: data.data.readings
+	}))
 );
+
 const schema = v.object({
-  pages: v.object({
-    next_url: v.optional(v.string()),
-  }),
-  data: v.array(subjectSchema),
+	pages: v.object({
+		next_url: v.optional(v.string())
+	}),
+	data: v.array(subjectSchema)
 });
+
+export type Subject = v.InferOutput<typeof subjectSchema>;
 
 export const getAllSubjects = query(async () => {
-  let nextUrl = "https://api.wanikani.com/v2/subjects";
-  let subjects = [];
+	let nextUrl: string | undefined = 'https://api.wanikani.com/v2/subjects';
+	let subjects: Subject[] = [];
 
-  while (nextUrl) {
-    const response = await fetchSubjects(nextUrl);
-    nextUrl = response.pages.next_url;
-  }
+	while (nextUrl) {
+		const response = await fetchSubjects(nextUrl);
+		subjects = [...subjects, ...response.data];
+		nextUrl = response.pages.next_url;
+	}
+
+	return subjects;
 });
 
-async function fetchSubjects(url: string): typeof schema {
-  const { cookies } = getRequestEvent();
-  const apiToken = CookieUtil.get(cookies, "api_token");
+async function fetchSubjects(url: string) {
+	const { cookies } = getRequestEvent();
+	const apiToken = CookieUtil.get(cookies, 'api_token');
 
-  if (!apiToken) {
-    throw new Error("Could not get user, no api key.");
-  }
+	if (!apiToken) {
+		throw new Error('Could not get user, no api key.');
+	}
 
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${apiToken}`,
-    },
-  });
+	const response = await fetch(url, {
+		headers: {
+			Authorization: `Bearer ${apiToken}`
+		}
+	});
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch subject (${response.status})`);
-  }
+	if (!response.ok) {
+		throw new Error(`Failed to fetch subjects (${response.status})`);
+	}
 
-  const json = await response.json();
-  return v.parse(schema, json);
+	const json = await response.json();
+	return v.parse(schema, json);
 }
--- src/lib/repository/subjectsRepository.ts
+++ src/lib/repository/subjectsRepository.ts
@@ -1,5 +1,31 @@
-import { Repository } from "$lib/repository/repository";
+import { getAllSubjects, type Subject } from '$lib/functions/subjects.remote';
+import { Repository } from '$lib/repository/repository';
+
+const KEY_PREFIX = 'subject.';
 
 export default class SubjectsRepository extends Repository {
-  public static async getSubject(id: string): Promise<any> {}
+	public static async getSubject(id: string): Promise<Subject | undefined> {
+		let subject: Subject | undefined = Repository.read(this.getKey(id));
+
+		if (!subject) {
+			// Delete all subjects from local storage
+			Object.keys(localStorage)
+				.filter((key) => key.startsWith(KEY_PREFIX))
+				.forEach((key) => localStorage.removeItem(key));
+
+			const allSubjects = await getAllSubjects();
+
+			for (const newSubject of allSubjects) {
+				Repository.write(this.getKey(String(newSubject.id)), newSubject);
+			}
+
+			subject = Repository.read(this.getKey(id));
+		}
+
+		return subject;
+	}
+
+	private static getKey(id: string): string {
+		return `${KEY_PREFIX}${id}`;
+	}
 }
