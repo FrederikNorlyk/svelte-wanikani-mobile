b7810d5af1a255e6a7d0788b25e5ea326799aeb1
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-27 20:49:17 +0100

    Added remote function to create a review

---
+++ src/lib/functions/reviews.remote.ts
@@ -0,0 +1,39 @@
+import { query } from '$app/server';
+import sendHTTPRequest from '$lib/util/httpUtil';
+import * as v from 'valibot';
+
+const schema = v.pipe(
+	v.object({
+		assignmentId: v.pipe(
+			v.number(),
+			v.integer('assignmentId must be an integer'),
+			v.minValue(1, 'assignmentId must be set')
+		),
+		incorrectMeaningAnswers: v.pipe(
+			v.number(),
+			v.integer('incorrectMeaningAnswers must be an integer'),
+			v.minValue(0, 'incorrectMeaningAnswers must be zero or a positive number')
+		),
+		incorrectReadingAnswers: v.pipe(
+			v.number(),
+			v.integer('incorrectReadingAnswers must be an integer'),
+			v.minValue(0, 'incorrectReadingAnswers must be zero or a positive number')
+		)
+	}),
+	v.transform(({ assignmentId, incorrectMeaningAnswers, incorrectReadingAnswers }) => ({
+		review: {
+			assignment_id: assignmentId,
+			incorrect_meaning_answers: incorrectMeaningAnswers,
+			incorrect_reading_answers: incorrectReadingAnswers
+		}
+	}))
+);
+
+export const createReview = query(schema, async (body) => {
+	await sendHTTPRequest('https://api.wanikani.com/v2/reviews/', {
+		method: 'POST',
+		body: body
+	});
+
+	return { success: true };
+});
