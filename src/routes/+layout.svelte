166a3362d39adf11b8a01580bb532ee16bc06e5d
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-29 18:20:36 +0100

    Remove redundant safe area padding and adjust main container box model

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,16 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	import { Toaster } from '$lib/shadcn/components/ui/sonner/index.js';
+
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
 <Toaster position="top-center" />
 
-<div class="flex min-h-screen flex-col space-y-2 p-5">
+<div class="box-border flex min-h-dvh flex-col space-y-2 p-5">
 	{@render children()}
 </div>
