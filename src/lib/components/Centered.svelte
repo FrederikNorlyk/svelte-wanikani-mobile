de48709ff38824793890159021ccc02856bbf846
FrederikNorlyk <frederiknorlyk@gmail.com>
2026-01-31 13:47:44 +0100

    Removed debug class

--- src/lib/components/Centered.svelte
+++ src/lib/components/Centered.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <div class="flex flex-1 items-center justify-center">
-	<div
-		class="flex flex-col items-center gap-3 text-center text-accent-foreground backdrop-grayscale-100"
-	>
+	<div class="flex flex-col items-center gap-3 text-center">
 		{@render children()}
 	</div>
 </div>
