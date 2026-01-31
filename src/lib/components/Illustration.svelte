eb21aa9fc9cc93a4f0fad79434e98a51070f8abf
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-31 11:39:14 +0100

    Enhance `Illustration` component with lazy loading, async decoding, and fade-in on load

--- src/lib/components/Illustration.svelte
+++ src/lib/components/Illustration.svelte
@@ -8,7 +8,23 @@
 	}
 
 	const { src, alt, children }: Props = $props();
+
+	let loaded = $state(false);
+
+	function markLoaded() {
+		loaded = true;
+	}
 </script>
 
-<img class="size-60" {src} {alt} />
+<img
+	class="size-60 transition-opacity duration-1000"
+	class:opacity-0={!loaded}
+	class:opacity-100={loaded}
+	{src}
+	{alt}
+	loading="lazy"
+	decoding="async"
+	onload={markLoaded}
+/>
+
 <p class="text-lg">{@render children()}</p>
