c4b2fc988d2fd508e6e7244c7a2a082f7a864eb1
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-01-31 11:41:10 +0100

    Refactor `Synchronizing.svelte` to use `Illustration` component with crab crane game image and update layout with overlayed spinner

--- src/lib/components/Synchronizing.svelte
+++ src/lib/components/Synchronizing.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
 	import { Spinner } from '$lib/shadcn/components/ui/spinner';
+	import crane_game_kani from '$lib/assets/irasutoya/crane_game_kani.png';
 	import Centered from '$lib/components/Centered.svelte';
+	import Illustration from '$lib/components/Illustration.svelte';
 </script>
 
 <Centered>
-	<Spinner class="size-44 text-muted-foreground" />
-	<p class="text-muted-foreground">Synchronizing with WaniKani</p>
+	<div class="relative">
+		<Illustration
+			src={crane_game_kani}
+			alt="Illustration of a crane game in Kushiro, Hokkaido where you can catch crabs"
+		>
+			Synchronizing with WaniKani
+		</Illustration>
+
+		<div
+			class="pointer-events-none absolute inset-0 z-10 flex items-center justify-center opacity-40"
+		>
+			<Spinner class="size-40 text-white" />
+		</div>
+	</div>
 </Centered>
