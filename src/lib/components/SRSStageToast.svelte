c17b011784b39d6c803015dfa02c600a2bfb672c
FrederikNorlyk <frederiknorlyk@gmail.com>
2026-02-01 13:02:23 +0100

    WIP: Make SRS toast only show on burned items + performance improvements

--- src/lib/components/SRSStageToast.svelte
+++ src/lib/components/SRSStageToast.svelte
@@ -1,26 +1,15 @@
 <script lang="ts">
 	import type { SRSStage } from '$lib/util/srsStageUtil';
 
-	export type Variant = 'success' | 'error';
-
 	interface Props {
 		srsStage: SRSStage;
-		variant: Variant;
 	}
 
-	const { srsStage, variant }: Props = $props();
+	const { srsStage }: Props = $props();
 </script>
 
-<div
-	class="mx-20 rounded-lg p-2"
-	class:bg-green-100={variant === 'success'}
-	class:bg-red-100={variant === 'error'}
->
-	<p
-		class="text-center"
-		class:text-green-500={variant === 'success'}
-		class:text-red-500={variant === 'error'}
-	>
+<div class="mx-20 rounded-lg bg-green-100 p-2">
+	<p class="text-center text-green-500">
 		{srsStage}
 	</p>
 </div>
