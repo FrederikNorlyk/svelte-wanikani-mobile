edaae628c3cf990445400a726ab6152d2d12581a
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-02-12 19:35:06 +0100

    Moved favicon to the correct directory and added an iOS specific icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	import { Toaster } from '$lib/shadcn/components/ui/sonner/index.js';
 
