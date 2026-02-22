bafc5cd0204ba960ef5fd460713c57afa2b9f132
Frederik Norlyk Guldborg <frederiknorlyk@gmail.com>
2026-02-22 12:14:56 +0100

    Fixed warnings

--- src/lib/functions/subjects.remote.ts
+++ src/lib/functions/subjects.remote.ts
@@ -105,7 +105,7 @@ export const getAllSubjects = query(async () => {
 		} catch (e) {
 			if (e instanceof ValiError && e.issues) {
 				const issue = e.issues[0];
-				throw new Error(issue.message);
+				throw new Error(issue.message, { cause: e });
 			}
 			throw e;
 		}
