98b41048e34c6fb1ab79408d80d0e08ec6dfa240
FrederikNorlyk <132744263+FrederikNorlyk@users.noreply.github.com>
2026-09-05 20:03:08 +0200

    Prevented vertical scroll bounce

--- src/lib/components/practice/ScrollableGrid.svelte
+++ src/lib/components/practice/ScrollableGrid.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="relative min-h-0 flex-1 rounded-md border bg-muted p-2">
-	<div class="h-full overflow-y-auto rounded-md">
+	<div
+		class="h-full touch-pan-y overflow-x-hidden overflow-y-auto overscroll-y-contain rounded-md"
+	>
 		<div
 			class="grid grid-cols-4 gap-2 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10"
 		>
